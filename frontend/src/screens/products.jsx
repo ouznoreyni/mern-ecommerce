1ade4afef6f52548e576f086e250875438dad0f2
Ousmane Diop <ousmanediopp268@gmail.com>
2021-05-23 13:58:22 +0000

    add product jsonserver

--- frontend/src/screens/products.jsx
+++ frontend/src/screens/products.jsx
@@ -1,49 +1,58 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import CardProduct from "../components/cardProduct";
-import Header from "../components/header/header";
-import MenuSideBar from "../components/menuSideBar";
-import Pagination from "../components/pagination";
-import data from "../data.json";
-import Footer from "./footer";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import CardProduct from '../components/cardProduct';
+import Header from '../components/header/header';
+import MenuSideBar from '../components/menuSideBar';
+import Pagination from '../components/pagination';
+import productsService from '../services/productsService';
+import Footer from './footer';
 
 const Products = () => {
-  const [products] = useState(data.products.slice(0, 8));
-  return (
-    <>
-      <Header />
-      <h1 className="text-3xl">Liste des produits</h1>
-      <div className="grid grid-cols-4 gap-4 mt-10">
-        {/* menu of the sidebar */}
-        <div className="xl:col-span-1 lg:col-span-1  col-span-4">
-          <MenuSideBar />
-        </div>
-        {/* end menu of the sidebar */}
-        {/* list products*/}
+	const [products, setProducts] = useState([]);
 
-        <div className="2xl:col-span-3 xl:col-span-3 lg:col-span-3  col-span-4 ">
-          <div className="pt-2 relative ml-7 inline-block mx-auto text-gray-600">
-            <input
-              className="border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
-              type="search"
-              name="search"
-              placeholder="Search"
-            />
-          </div>
-          <div className="inline-block float-right mr-5">
-            <button className="relative text-sm focus:outline-none group mt-4 sm:mt-0">
-              <div className="flex items-center justify-between w-40 h-10 px-3 border-2 border-gray-300 rounded hover:bg-gray-300">
-                <span className="font-medium">Popular</span>
-                <i className="fas fa-caret-down w-4 h-4"></i>
-              </div>
-              <div className="absolute z-10 flex-col items-start hidden w-full pb-1 bg-white shadow-lg rounded group-focus:flex">
-                <Link
-                  to="#"
-                  className="w-full px-4 py-2 text-left hover:bg-gray-200"
-                >
-                  Popular
-                </Link>
-                {/* <a
+	const loadProducts = async () => {
+		const data = await productsService.getAll();
+		setProducts(data.data);
+	};
+
+	useEffect(() => {
+		loadProducts();
+	}, []);
+	return (
+		<>
+			<Header />
+			<h1 className='text-3xl'>Liste des produits</h1>
+			<div className='grid grid-cols-4 gap-4 mt-10'>
+				{/* menu of the sidebar */}
+				<div className='xl:col-span-1 lg:col-span-1  col-span-4'>
+					<MenuSideBar />
+				</div>
+				{/* end menu of the sidebar */}
+				{/* list products*/}
+
+				<div className='2xl:col-span-3 xl:col-span-3 lg:col-span-3  col-span-4 '>
+					<div className='pt-2 relative ml-7 inline-block mx-auto text-gray-600'>
+						<input
+							className='border-2 border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none'
+							type='search'
+							name='search'
+							placeholder='Search'
+						/>
+					</div>
+					<div className='inline-block float-right mr-5'>
+						<button className='relative text-sm focus:outline-none group mt-4 sm:mt-0'>
+							<div className='flex items-center justify-between w-40 h-10 px-3 border-2 border-gray-300 rounded hover:bg-gray-300'>
+								<span className='font-medium'>Popular</span>
+								<i className='fas fa-caret-down w-4 h-4'></i>
+							</div>
+							<div className='absolute z-10 flex-col items-start hidden w-full pb-1 bg-white shadow-lg rounded group-focus:flex'>
+								<Link
+									to='#'
+									className='w-full px-4 py-2 text-left hover:bg-gray-200'
+								>
+									Popular
+								</Link>
+								{/* <a
                   class="w-full px-4 py-2 text-left hover:bg-gray-200"
                   href="#"
                 >
@@ -67,21 +76,21 @@ const Products = () => {
                 >
                   Highest Price
                 </a> */}
-              </div>
-            </button>
-          </div>
-          <div className="grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-6 gap-y-12 w-full mt-6  p-8">
-            {products.map((p) => (
-              <CardProduct key={p.id} product={p} />
-            ))}
-          </div>
-          <Pagination />
-        </div>
-        {/* end list products*/}
-      </div>
-      <Footer />
-    </>
-  );
+							</div>
+						</button>
+					</div>
+					<div className='grid 2xl:grid-cols-4 xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-x-6 gap-y-12 w-full mt-6  p-8'>
+						{products.map((p) => (
+							<CardProduct key={p.id} product={p} />
+						))}
+					</div>
+					<Pagination />
+				</div>
+				{/* end list products*/}
+			</div>
+			<Footer />
+		</>
+	);
 };
 
 export default Products;
--- frontend/src/services/httpService.js
+++ frontend/src/services/httpService.js
@@ -1,28 +1,28 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: process.env.REACT_APP_API_URL,
-  timeout: 1000,
+	baseURL: process.env.REACT_APP_API_URL_MOCK,
+	timeout: 1000,
 });
 
 // Add a response interceptor
 instance.interceptors.response.use(null, (error) => {
-  if (error.response) {
-    console.log(error.response.data);
-  } else {
-    console.log(error.message);
-  }
-  return Promise.reject(error);
+	if (error.response) {
+		console.log(error.response.data);
+	} else {
+		console.log(error.message);
+	}
+	return Promise.reject(error);
 });
 
 function setJWT(jwt) {
-  instance.defaults.headers.common['x-auth-token'] = jwt;
+	instance.defaults.headers.common['x-auth-token'] = jwt;
 }
 
-const httpService={
-  setJWT,
-  get: instance.get,
-  post: instance.post,
-  put: instance.put,
-  delete: instance.delete,
+const httpService = {
+	setJWT,
+	get: instance.get,
+	post: instance.post,
+	put: instance.put,
+	delete: instance.delete,
 };
-export default  httpService;
+export default httpService;
