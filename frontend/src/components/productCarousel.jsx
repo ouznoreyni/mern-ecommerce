d74cf66f0e5a3ad3cb51f861009a49c210adc021
Ousmane Diop <ousmanediopp268@gmail.com>
2021-06-06 09:32:40 +0000

    fix issues

--- frontend/src/components/productCarousel.jsx
+++ frontend/src/components/productCarousel.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import Slider from "react-slick";
-import "slick-carousel/slick/slick-theme.css";
-import "slick-carousel/slick/slick.css";
-import data from "../data.json";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick-theme.css';
+import 'slick-carousel/slick/slick.css';
+import data from '../data.json';
 
 const ProductCarousel = () => {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-  };
-  const [products] = useState(data.products.slice(0, 3));
+	var settings = {
+		dots: true,
+		infinite: true,
+		speed: 500,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		autoplay: true,
+	};
+	const [products] = useState(data.products.slice(0, 3));
 
-  return (
-    <Slider {...settings}>
-      {products.map((product) => (
-        <div key={product.name}>
-          <Link
-            to="#"
-            className="block h-80 rounded-lg shadow-lg bg-gray-600"
-            style={{
-              backgroundImage: `url(${product.image})`,
-              backgroundSize: "cover",
-            }}
-          ></Link>
-        </div>
-      ))}
-    </Slider>
-  );
+	return (
+		<Slider {...settings}>
+			{products.map((product) => (
+				<div key={product.name}>
+					<Link
+						to='#'
+						className='block h-80 rounded-lg shadow-lg bg-gray-600'
+						style={{
+							backgroundImage: `url(${product.image})`,
+							backgroundSize: 'cover',
+						}}
+					></Link>
+				</div>
+			))}
+		</Slider>
+	);
 };
 
 export default ProductCarousel;
